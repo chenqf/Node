8d38f438c8c278a41ba37e0ecef21580a1172c66
chenqf <qifeng.chen1006@gmail.com>
2015-11-04 23:05:14 +0800

    express 学习

--- routes/mall.js
+++ routes/mall.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var mongoConfig = require('../config/mongoConfig.json');
 
 /**
  * 中间件，所有router下的请求，都会先走这个方法
@@ -11,6 +13,20 @@ router.use(function timeLog(req, res, next) {
 
 router.get('/',function(req,res,next){
     console.log("cookie: " + JSON.stringify(req.cookies));
+
+    //连接数据库，ip，port
+    var server = new mongo.Server(mongoConfig.url,mongoConfig.port);
+    //获取数据库对象，指定数据库名称
+    var db = new mongo.Db(mongoConfig.databaseName,server);
+    db.open(function(error,db){
+        console.log(db);
+        console.log("db open ok")
+    });
+    //不强制关闭数据库，关闭后，还可通过open打开
+    /*db.close(false,function(){
+        console.log('db close ok');
+    })*/
+
     res.send('router get /');
 });
 
