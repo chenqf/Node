c5b34058d9cf72277b8539b2b73be5e7a630d08b
chenqf <qifeng.chen1006@gmail.com>
2015-12-21 15:49:50 +0800

    node 邮件，node定时

--- config/config.js
+++ config/config.js
@@ -3,7 +3,12 @@
  */
 
 var config = {
-    "port":3000
+    "port":3000,
+    "mail":{
+        "username":"546710115@qq.com",
+        "password":"cqf5115215473",
+        "fromName":"陈其丰 <546710115@qq.com>"
+    }
 };
 if(process.env.NODE_ENV === 'preOnline'){
     config.domain = '66buy.com'
